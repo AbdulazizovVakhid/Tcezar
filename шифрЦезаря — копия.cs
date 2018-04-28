e16d6d9accfb6ece0674a481f33d66d96345fb0e
Vakhid <vahidabdulazizov@MacBook-Pro-Vahid.local>
2018-04-28 20:32:22 +0300

    все так?

---
+++ шифрЦезаря — копия.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+namespace шифр_Цезаря
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine();
+            string crypt = CaesarCipher.Encryption(text);
+            Console.WriteLine(" шифр: {0}", crypt);
+            Console.ReadLine();
+        }
+    }
+    class CaesarCipher
+    {
+        static string alph = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+
+        public static string Encryption(string text)
+        {
+            var res = new StringBuilder();
+            for (int i = 0; i < text.Length; i++)
+                for (int j = 0; j < alph.Length; j++)
+                    if (text[i] == alph[j]) res.Append(alph[(j + 3) % alph.Length]);
+
+            return res.ToString();
+        }
+        public static string Decryption(string crypt)
+        {
+            var res = new StringBuilder();
+            for (int i = 0; i < crypt.Length; i++)
+                for (int j = 0; j < alph.Length; j++)
+                    if (crypt[i] == alph[j]) res.Append(alph[(j - 3 + alph.Length) % alph.Length]);
+
+            return res.ToString();
+        }
+    }
+}
